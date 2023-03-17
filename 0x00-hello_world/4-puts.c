88a511e5a7ddf357af479c9abfddd6b14ad7541c
DAMILARE <damilareawotola@gmail.com>
2023-03-17 11:23:39 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
-  1 #include <stdio.h>
-  2 int main(void)
-  3 {
-  4       puts("\"Programming is like building a multilingual puzzle");
-  5       return 0;
-  6 }
+#include <stdio.h>
+/**
+ * main - Programming is like building a multilingual puzzle
+ * return: #
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (o);
+}
