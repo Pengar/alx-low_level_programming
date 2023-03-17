84fffd8e3e892a348d8c6f67820b83ce566f30b1
DAMILARE <damilareawotola@gmail.com>
2023-03-17 11:04:06 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
- 1 #include <stdio.h>
- 2 int main(voi)
- 3 {
- 4       puts("\"programming is cool");
- 5       return 0;
- 6 }
+  1 #include <stdio.h>
+  2 int main(void)
+  3 {
+  4       puts("\"Programming is like building a multilingual puzzle");
+  5       return 0;
+  6 }
