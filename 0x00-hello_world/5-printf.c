d56ad857707e945bd1c71c18ae26323ef9d313f3
DAMILARE <damilareawotola@gmail.com>
2023-03-18 03:26:58 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+  * Description: main - print out strings stdout.
+  * Return: always 0 if success.
+ */
+int main(void)
+{
+puts("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
