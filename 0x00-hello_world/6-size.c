5d761e2b4bccee39bf554333408ea4381121d0f3
DAMILARE <damilareawotola@gmail.com>
2023-03-18 10:34:01 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,9 +4,9 @@
  *
  * Return: 0 (success)
  */
-
 int main(void)
-{	
+{
+
 printf("Size of a char: %d byte(s)\n", sizeof(char));
 printf("size of an int: %d byte(s)\n", sizeof(int));
 printf("size of a long int: %d byte(s)\n", sizeof(long int));
