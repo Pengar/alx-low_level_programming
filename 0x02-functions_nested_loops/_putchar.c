4d8e208f7737a477f5e2683624ea8d53d24d7e1e
DAMILARE <damilareawotola@gmail.com>
2023-03-23 04:38:18 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - write the character c to stdout
+ *  The character to print c
+ *
+ *  return: -1 is returned, and error is set appropriately.
+ */
+
+int _purchar(c)
+{
+	Return(write(1, &c, 1));
+
+}
