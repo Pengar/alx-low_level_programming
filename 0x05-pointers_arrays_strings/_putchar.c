dd0eed438bac8fbe475b082afb9acb7cd612b198
DAMILARE <damilareawotola@gmail.com>
2023-03-27 14:11:04 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @char *str: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char *str)
+{
+	return (write(1, &c, 1));
+}
+
+
