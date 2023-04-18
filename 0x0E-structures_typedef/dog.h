bbbbd42ec9afb8f1817315a4839ae32d48ef503a
DAMILARE <damilareawotola@gmail.com>
2023-04-17 17:49:26 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - Dog
+ * @name: dog name
+ * @age: age of the dog
+ * @owner:dog owner
+ * Desc: header file
+ */
+
 typedef struct dog
 {
 char *name;
