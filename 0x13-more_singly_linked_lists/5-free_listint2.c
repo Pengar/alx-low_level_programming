0c0d37380d00efd0c2bb4f72dea182f7ec49b516
pengar <Damilareawotola@gmail.com>
2023-05-02 14:21:53 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ *free_listint2 - frees linked list
+ *@head: head pointer
+ *
+ *Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *cursor;
+	listint_t **temp = head;
+
+	if (temp != NULL)
+	{
+		while (*head != NULL)
+		{
+			cursor = *head;
+			free(cursor);
+			*head = (*head)->next;
+		}
+
+		*temp = NULL;
+	}
+}
