60215e62fced46c54267cb0ddbeb8003d5449c72
pengar <Damilareawotola@gmail.com>
2023-05-02 16:05:29 -0700

    10-delete_nodeint.c

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,58 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes  node at a given index
+ * @head: head pointer
+ * @index: added node index
+ *
+ * Return: the address of the node
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *old_node = NULL;
+	listint_t *previous_node = NULL;
+	unsigned int k = 0, list_len = listint_len(*head);
+
+	if ((index > list_len) || (list_len == 0))
+		return (-1);
+	while (head != NULL)
+	{
+		if (k == index)
+		{
+			old_node = *head;
+			if (k == 0)
+			{
+				*head = old_node->next;
+				free(old_node);
+				return (1);
+			}
+			previous_node->next = old_node->next;
+			free(old_node);
+			return (1);
+		}
+		else if ((k + 1) == index)
+			previous_node = *head;
+		head = &((*head)->next);
+		k++;
+	}
+	return (-1);
+}
+
+/**
+ * listint_len - counts the number of nodes in a linked list
+ * @h: head of the list
+ *
+ * Return: the number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *cursor = h;
+	size_t count = 0;
+
+	while (cursor != NULL)
+	{
+		count += 1;
+		cursor = cursor->next;
+	}
+	return (count);
+}
