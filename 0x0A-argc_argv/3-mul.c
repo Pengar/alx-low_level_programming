1b0a6e6c0063e7ff1498d8d3bcbda179c530f0bb
Pengar <Damilareawotola@gmail.com>
2023-08-04 08:28:50 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *@argc: arguments count
+ *@argv: arguments vetor
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int n1 = 0, n2 = 0;
+
+	if (argc == 3)
+	{
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
+		printf("%d\n", n1 * n2);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}
