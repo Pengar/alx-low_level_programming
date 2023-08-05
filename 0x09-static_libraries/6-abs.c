8a0b1a14a7ad4d4dfad15485a740a745197c8684
Pengar <Damilareawotola@gmail.com>
2023-08-04 21:39:55 -0700

    libmy

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *_abs - absolute value
+ *
+ *@r: the value stored in x through the conditional
+ *
+ *main - printing the value of a number
+ *
+ *Return: The value will return 0
+ */
+
+int _abs(int r)
+
+{
+	if (r > 0)
+	{
+		return (r);
+	}
+	else
+	{
+		return (r * -1);
+	}
+}
