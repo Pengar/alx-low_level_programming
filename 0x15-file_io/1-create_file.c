a428496b043067ed3cde443071d0e44005d6fdd5
Pengar <Damilareawotola@gmail.com>
2023-09-04 07:32:51 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,30 +16,30 @@ int create_file(const char *filename, char *text_content)
 {
 
 	int fd, sz, i;
-	char *buf;
+	char *pen;
 
 	if (filename == NULL)
 		return (-1);
 	if (text_content == NULL)
 	{
-		fd = open(filename, O_CREAT, 0700);
+		fd = open(filename, O_CREAT, 0600);
 		if (fd == -1)
 			return (-1);
 		return (1);
 	}
 	for (i = 0; text_content[i] != '\0'; i++)
 		;
-	buf = malloc(i * sizeof(char));
-	if (buf == NULL)
+pen = malloc(i * sizeof(char));
+	if (pen == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0700);
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (fd == -1)
 		return (-1);
 	sz = write(fd, text_content, i);
 	if (sz == -1)
 		return (-1);
 	close(fd);
-	free(buf);
+	free(pen);
 	return (1);
 
 
